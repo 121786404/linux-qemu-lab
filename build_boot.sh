a0fcf1a2ef9ab3b9aba4d1e9f3aec966ed08d2c4
sheldon.liu <ljb861213@163.com>
2016-08-21 20:11:04 +0800

    update

--- build_boot.sh
+++ build_boot.sh
@@ -3,5 +3,5 @@ export ARCH=arm
 export CROSS_COMPILE=arm-linux-gnueabi-
 make vexpress_ca9x4_defconfig
 make
-../tools/collect-src -o ../boot_src.list
+../tools/collect-src -f "u-boot" -o ../boot_src.list
 cd ..
