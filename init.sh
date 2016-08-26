0b8500c252de31ceddff166bd366b52659c9d169
sheldon.liu <sheldon.liu@mstarsemi.com>
2016-08-26 14:39:37 +0000

    add

--- init.sh
+++ init.sh
@@ -42,12 +42,12 @@ source ./build_busybox.sh >/dev/null 2>&1
 
 
 sudo cp tools/tftpd-hpa.cfg /etc/default/tftpd-hpa
-
-sudo brctl addbr br0
-sudo brctl addif br0 ens33
-sudo ifconfig ens33 down
-sudo ifconfig ens33 0.0.0.0 promisc up
-sudo ifconfig br0 10.0.0.1 up
-sudo route add default gw 10.0.0.1 br0
+sudo cp tools/qemu-ifup /etc
+#sudo brctl addbr br0
+#sudo brctl addif br0 ens33
+#sudo ifconfig ens33 down
+#sudo ifconfig ens33 0.0.0.0 promisc up
+#sudo ifconfig br0 10.0.0.1 up
+#sudo route add default gw 10.0.0.1 br0
 
 
