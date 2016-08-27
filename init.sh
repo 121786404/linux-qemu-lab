97d9a28083a3a2af19e8452746408ccbcfb9aa29
sheldon.liu <ljb861213@163.com>
2016-08-27 22:28:07 +0800

    add submoudle

--- init.sh
+++ init.sh
@@ -1,29 +1,5 @@
 #!/bin/bash
 
-SOURCE_DIR="source"
-
-KERNEL_NAME="linux-4.4.18"
-BUSYBOX_NAME="busybox-1.25.0"
-UBOOT_NAME="u-boot-2016.07"
-KERNEL_FILE="$KERNEL_NAME.tar.xz"
-BUSYBOX_FILE="$BUSYBOX_NAME.tar.bz2"
-UBOOT_FILE="$UBOOT_NAME.tar.bz2"
-
-
-echo "========begin extract source========="
-
-
-rm -rf linux u-boot busybox
-rm -rf $KERNEL_NAME $BUSYBOX_NAME $UBOOT_NAME
-
-tar xf "$SOURCE_DIR/$KERNEL_FILE"
-mv $KERNEL_NAME linux
-
-tar xf "$SOURCE_DIR/$BUSYBOX_FILE"
-mv $BUSYBOX_NAME busybox
-
-tar xf "$SOURCE_DIR/$UBOOT_FILE"
-mv $UBOOT_NAME u-boot
 
 
 echo "========begin build u-boot========="
