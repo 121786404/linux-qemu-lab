c83ff8f1f11390f532736b3a5211545745656460
sheldon.liu <ljb861213@163.com>
2016-08-30 01:30:31 +0800

    update u-boot script

--- build_boot.sh
+++ build_boot.sh
@@ -6,14 +6,13 @@ export CROSS_COMPILE=arm-linux-gnueabi-
 
 
 
-git reset --hard HEAD
-git clean -f
+git checkout include/configs/vexpress_common.h
 sed -i '/\#endif \/\* VEXPRESS_COMMON_H \*\// i \
 #undef CONFIG_BOOTCOMMAND \
 #define CONFIG_IPADDR		10.0.0.2 \
 #define CONFIG_SERVERIP		10.0.0.1 \
 #define CONFIG_BOOTCOMMAND	"tftp 0x60003000 linux-qemu-lab/image/uImage;tftp 0x60500000 linux-qemu-lab/image/vexpress-v2p-ca9.dtb;bootm 0x60003000 - 0x60500000" \
-#define CONFIG_BOOTARGS	    "init=/linuxrc console=ttyAMA0 root=/dev/mmcblk0 rw rootwait" \
+#define CONFIG_BOOTARGS	    "init=/linuxrc console=ttyAMA0,115200 root=/dev/mmcblk0 rw rootwait" \
 ' include/configs/vexpress_common.h
 
 make vexpress_ca9x4_defconfig
@@ -22,14 +21,13 @@ cp u-boot ../image/u-boot
 ../tools/collect-src -f "u-boot" -o ../boot_src.list
 
 
-git reset --hard HEAD
-git clean -f
+git checkout include/configs/vexpress_common.h
 sed -i '/\#endif \/\* VEXPRESS_COMMON_H \*\// i \
 #undef CONFIG_BOOTCOMMAND \
 #define CONFIG_IPADDR		10.0.0.2 \
 #define CONFIG_SERVERIP		10.0.0.1 \
 #define CONFIG_BOOTCOMMAND	"tftp 0x60003000 linux-qemu-lab/image/uImage;tftp 0x60500000 linux-qemu-lab/image/vexpress-v2p-ca9.dtb;bootm 0x60003000 - 0x60500000" \
-#define CONFIG_BOOTARGS	    "init=/linuxrc root=/dev/mmcblk0 rw rootwait" \
+#define CONFIG_BOOTARGS	    "init=/linuxrc kgdboc=ttySAC0,115200 kgdbwait root=/dev/mmcblk0 rw rootwait" \
 ' include/configs/vexpress_common.h
 
 make vexpress_ca9x4_defconfig
