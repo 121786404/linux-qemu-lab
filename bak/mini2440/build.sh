dd933365d03938a2b01ec102a3377d9106521372
sheldon.liu <ljb861213@163.com>
2016-09-04 18:45:05 +0800

    update mini2440

--- bak/mini2440/build.sh
+++ bak/mini2440/build.sh
@@ -1,1 +1,2 @@
-./configure --target-list=arm-softmmu --disable-vnc-tls
+#./configure --target-list=arm-softmmu --disable-vnc-tls
+./configure --target-list=arm-softmmu --disable-vnc-tls --disable-curses
