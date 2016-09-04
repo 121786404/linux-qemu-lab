c5d68b2e554d46e49b2339d5e10b75151ba86ea4
sheldon.liu <ljb861213@163.com>
2016-09-04 19:59:10 +0800

    update build_qemu

--- bak/mini2440/build_qemu.sh
+++ bak/mini2440/build_qemu.sh
@@ -1,2 +1,4 @@
+sudo apt-get install libsdl1.2debian:i386
+#sudo apt-get install libsdl1.2:i386
 #./configure --target-list=arm-softmmu --disable-vnc-tls
 ./configure --target-list=arm-softmmu --disable-vnc-tls --disable-curses
