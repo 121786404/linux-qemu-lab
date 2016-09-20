89c4422d91d048e3b110ca48c63c78d6988df24d
sheldon.liu <ljb861213@163.com>
2016-09-20 13:59:52 +0800

    can owrk

--- run.sh
+++ run.sh
@@ -7,6 +7,14 @@ source tools/kill_qemu.sh
 qemu-system-i386 -M pc \
 -kernel linux/arch/x86/boot/bzImage \
 -drive file=tools/x86rootfs.ext2,if=virtio,format=raw \
--append root=/dev/vda \
+-append "root=/dev/vda" \
 -net nic,model=virtio \
 -net user
+
+exit
+
+qemu-system-i386 -M pc \
+-nographic \
+-kernel linux/arch/x86/boot/bzImage \
+-hda tools/x86rootfs.ext2 \
+-append "root=/dev/sda console=ttyS0"
