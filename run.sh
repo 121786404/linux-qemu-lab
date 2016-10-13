6979cecee8873ce9429b619a9ad0e1c284d15fe5
sheldon.liu <ljb861213@163.com>
2016-10-13 21:10:51 +0800

    update

--- run.sh
+++ run.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 
 
-
+rm -f serial.out
 source tools/kill_qemu.sh
 
 qemu-system-i386 -M pc \
+--no-reboot \
 -kernel linux/arch/x86/boot/bzImage \
 -drive file=tools/x86rootfs.ext2,if=virtio,format=raw \
--append "root=/dev/vda" \
+-append "root=/dev/vda rw debug console=ttyS0" \
+-serial file:serial.out \
 -net nic,model=virtio \
 -net user
 
