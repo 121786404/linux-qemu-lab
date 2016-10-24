7fd75ee397aa69ea2e10d412a49c5d0a5bfc76a8
sheldon.liu <ljb861213@163.com>
2016-10-24 14:58:14 +0800

    update

--- tools/root_fs_patch/mount_nfs.sh
+++ tools/root_fs_patch/mount_nfs.sh
@@ -1,3 +1,3 @@
 ifconfig eth0 10.0.0.2
 mkdir -p share
-mount -t nfs -o nolock 10.0.0.1:/home/sheldon.liu/linux-qemu-lab/root_nfs/share   /share
+mount -t nfs -o nolock 10.0.0.1:/home/sheldon.liu/linux-qemu-lab-arm/root_nfs/share   /share
