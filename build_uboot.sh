af0f503c5dbee4115d0eab55fe6bac23ae290e23
sheldon.liu <ljb861213@163.com>
2017-05-09 11:23:57 +0800

    update

--- build_uboot.sh
+++ build_uboot.sh
@@ -11,7 +11,7 @@ sed -i '/\#endif \/\* VEXPRESS_COMMON_H \*\// i \
 #define CONFIG_IPADDR		10.0.0.2 \
 #define CONFIG_SERVERIP		10.0.0.1 \
 #define CONFIG_BOOTCOMMAND	"tftp 0x60003000 linux-qemu-lab-arm/image/uImage;tftp 0x60500000 linux-qemu-lab-arm/image/vexpress-v2p-ca9.dtb;bootm 0x60003000 - 0x60500000" \
-#define CONFIG_BOOTARGS	    "init=/linuxrc earlyprintk console=ttyAMA0,115200 root=/dev/mmcblk0 rw rootwait" \
+#define CONFIG_BOOTARGS	    "init=/linuxrc user_debug=0xff earlyprintk console=ttyAMA0,115200 root=/dev/mmcblk0 rw rootwait" \
 ' include/configs/vexpress_common.h
 
 make vexpress_ca9x4_defconfig
@@ -26,7 +26,7 @@ sed -i '/\#endif \/\* VEXPRESS_COMMON_H \*\// i \
 #define CONFIG_IPADDR		10.0.0.2 \
 #define CONFIG_SERVERIP		10.0.0.1 \
 #define CONFIG_BOOTCOMMAND	"tftp 0x60003000 linux-qemu-lab-arm/image/uImage;tftp 0x60500000 linux-qemu-lab-arm/image/vexpress-v2p-ca9.dtb;bootm 0x60003000 - 0x60500000" \
-#define CONFIG_BOOTARGS	    "init=/linuxrc kgdboc=ttySAC0,115200 kgdbwait root=/dev/mmcblk0 rw rootwait" \
+#define CONFIG_BOOTARGS	    "init=/linuxrc user_debug=0xff kgdboc=ttySAC0,115200 kgdbwait root=/dev/mmcblk0 rw rootwait" \
 ' include/configs/vexpress_common.h
 
 make vexpress_ca9x4_defconfig
