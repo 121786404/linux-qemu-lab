c734f72c914364c4571cf684b44342976395b548
sheldon.liu <ljb861213@163.com>
2017-05-13 22:26:38 +0800

    remove u-boot

--- run-uboot.sh
+++ run-uboot.sh
@@ -8,13 +8,10 @@ source tools/restart-net-servers.sh
 
 
 sudo qemu-system-arm \
--smp 2 \
 -nographic \
 -sd a9rootfs.ext2 \
 -M vexpress-a9 \
 -m 64M \
--kernel image/zImage \
--dtb image/vexpress-v2p-ca9.dtb \
--append "init=/linuxrc user_debug=0xff earlyprintk console=ttyAMA0,115200 root=/dev/mmcblk0 rw rootwait" \
+-kernel image/u-boot \
 -net nic,vlan=0 \
 -net tap,vlan=0,ifname=tap0,script=./tools/qemu-ifup,downscript=./tools/qemu-ifdown |tee linux.log
