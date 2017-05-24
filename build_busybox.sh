686af22e5bf41a0fdb3938f6fb03c95f7780b581
sheldon.liu <ljb861213@163.com>
2017-05-24 14:57:19 +0800

    update rootfs

--- build_busybox.sh
+++ build_busybox.sh
@@ -11,14 +11,10 @@ make install ARCH=arm CROSS_COMPILE=arm-linux-gnueabi-
 
 cd _install
 
-
 mkdir proc sys tmp root var mnt dev lib
 
-cp ../../tools/root_fs_patch/* . -rd
-cp /opt/gcc-linaro-4.9-2016.02-x86_64_arm-linux-gnueabi/arm-linux-gnueabi/libc/lib/*.so* lib -d
-
+cp /usr/arm-linux-gnueabi/lib/*.so* lib -d
 arm-linux-gnueabi-strip lib/*
-
 
 sudo mknod dev/tty1 c 4 1
 sudo mknod dev/tty2 c 4 2
@@ -26,7 +22,6 @@ sudo mknod dev/tty3 c 4 3
 sudo mknod dev/tty4 c 4 4
 sudo mknod dev/console c 5 1
 sudo mknod dev/null c 1 3
-
 
 cd ../..
 
--- tools/mkdisk.sh
+++ tools/mkdisk.sh
@@ -6,13 +6,12 @@
 #
 sudo rm -f a9rootfs.ext2
 dd if=/dev/zero of=a9rootfs.ext2 bs=1M count=32
-#dd if=/dev/zero of=a9rootfs.ext2 bs=1M count=200
 sudo mkfs.ext2 a9rootfs.ext2
 sudo rm -rf tmpfs
 mkdir -p tmpfs
 sudo mount -t ext2 a9rootfs.ext2 tmpfs -o loop
 sudo cp -raf busybox/_install/*  tmpfs
-#sudo cp -raf rootfs_qtopia_qt4/*  tmpfs
+cp tools/root_fs_patch/* tmpfs -rd
 sudo cp -r root_nfs/share tmpfs
 sudo umount tmpfs
 sudo rm -rf tmpfs
