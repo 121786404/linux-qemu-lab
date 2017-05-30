ba16d0ee9e1a662e5514e16b874b0b585f56a4bf
sheldon.liu <ljb861213@163.com>
2017-05-30 20:32:02 +0800

    add

--- run.sh
+++ run.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 
-
 source tools/kill_qemu.sh
 source tools/mkdisk.sh
 source tools/restart-net-servers.sh
@@ -18,3 +17,7 @@ sudo qemu-system-arm \
 -append "init=/linuxrc user_debug=0xff earlyprintk console=ttyAMA0,115200 root=/dev/mmcblk0 rw rootwait" \
 -net nic,vlan=0 \
 -net tap,vlan=0,ifname=tap0,script=./tools/qemu-ifup,downscript=./tools/qemu-ifdown |tee linux.log
+
+
+
+#-usbdevice host:1b3b:2977
